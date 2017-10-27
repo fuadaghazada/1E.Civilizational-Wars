415cc57a67c6665a7ddb76380175e75c024312bf
Fuad Aghazada <fuad.aghazada98@gmail.com>
2017-10-27 10:00:51 +0300

    Tile.class is added

--- src/game_object/map/Tile.java
+++ src/game_object/map/Tile.java
@@ -1,4 +1,81 @@
 package game_object.map;
 
-public class Tile {
+/**
+ *  This class create a tile for the map which contain of the tails.
+ *
+ *  @authors:
+ *      Fuad Aghazada
+ *
+ *
+ *
+ *  @version - 1.00
+ */
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class Tile
+{
+    //Constants
+
+    //Properties
+    private BufferedImage tileImage;
+
+    private float x, y;
+    private static int tileWidth, tileHeight;
+
+    /**
+     *  Constructs a tile with the given parameter of the image
+     *  @param tileImage - given image for the tile
+     */
+    public Tile(float x, float y, BufferedImage tileImage)
+    {
+        this.tileImage = tileImage;
+    }
+
+    /*
+     *	Test constructor
+     */
+    public Tile(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+
+        tileWidth = 700 / 15;
+        tileHeight = tileWidth;
+    }
+
+    /**
+     *	Updating the tile properties.
+     */
+    public void update()
+    {
+
+    }
+
+    /**
+     * Rendering the tile in terms of its properties.
+     */
+    public void render(Graphics2D g2)
+    {
+        g2.fillRect((int) x, (int) y, tileWidth, tileHeight);
+    }
+
+    // ACCESS & MUTATE
+
+    public BufferedImage getTileImage()
+    {
+        return tileImage;
+    }
+
+    public void setTileImage(BufferedImage tileImage)
+    {
+        this.tileImage = tileImage;
+    }
+
+    public static int getTileSize()
+    {
+        return tileWidth;
+    }
 }
+
